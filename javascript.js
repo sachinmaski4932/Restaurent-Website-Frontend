47574938e4775b15bd42b305fdbd1e9d6474e944
sachinmaski4932 <112940168+sachinmaski4932@users.noreply.github.com>
2022-10-16 20:42:18 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+});
+
